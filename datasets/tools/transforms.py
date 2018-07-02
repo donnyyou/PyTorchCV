a6f3ca58b76ec7912f726d2cdd8a1221baacd707
Donny You <youansheng@gmail.com>
2018-07-02 10:17:57 +0800

    blob helper.

--- datasets/tools/transforms.py
+++ datasets/tools/transforms.py
@@ -10,6 +10,7 @@ from __future__ import print_function
 import numpy as np
 import torch
 from PIL import Image
+import collections
 
 
 class Normalize(object):
@@ -79,6 +80,35 @@ class ToTensor(object):
             inputs = torch.from_numpy(inputs.transpose(2, 0, 1))
 
         return inputs.float()
+
+
+class Scale(object):
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, img):
+        assert isinstance(img, Image.Image)
+
+        if isinstance(self.size, int):
+            w, h = img.size
+            if (w >= h and w == self.size) or (h >= w and h == self.size):
+                return img
+
+            if w > h:
+                ow = self.size
+                oh = int(self.size * h / w)
+                return img.resize((ow, oh), Image.BILINEAR)
+            else:
+                oh = self.size
+                ow = int(self.size * w / h)
+                return img.resize((ow, oh), Image.BILINEAR)
+
+        elif isinstance(self.size, collections.Iterable) and len(self.size) == 2:
+            img = img.resize(self.size, Image.BILINEAR)
+            return img
+
+        else:
+            raise TypeError('Got inappropriate size arg: {}'.format(self.size))
 
 
 class Compose(object):
