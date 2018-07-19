e866fc3ab01ed0fdb3093cc4e503b6cce04b490c
Donny You <youansheng@gmail.com>
2018-07-19 13:51:41 +0800

    gitignore.

--- extensions/make.sh
+++ extensions/make.sh
@@ -2,8 +2,8 @@
 
 CUDA_PATH=/usr/local/cuda/
 
-python setup.py build_ext --inplace
-rm -rf build
+# python setup.py build_ext --inplace
+# rm -rf build
 
 # Choose cuda arch as you need
 CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
@@ -15,7 +15,7 @@ CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
 #          -gencode arch=compute_70,code=sm_70 "
 
 # compile NMS
-cd model/nms/src
+cd layers/nms/src
 echo "Compiling nms kernels by nvcc..."
 nvcc -c -o nms_cuda_kernel.cu.o nms_cuda_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
@@ -24,14 +24,14 @@ cd ../
 python build.py
 
 # compile roi_pooling
-cd ../../
-cd model/roi_pooling/src
+cd ../
+cd roi/roi_pooling/src
 echo "Compiling roi pooling kernels by nvcc..."
 nvcc -c -o roi_pooling.cu.o roi_pooling_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../
 python build.py
-
+cd ../
 # # compile roi_align
 # cd ../../
 # cd model/roi_align/src
@@ -42,19 +42,18 @@ python build.py
 # python build.py
 
 # compile roi_crop
-cd ../../
-cd model/roi_crop/src
+cd roi_crop/src
 echo "Compiling roi crop kernels by nvcc..."
 nvcc -c -o roi_crop_cuda_kernel.cu.o roi_crop_cuda_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../
 python build.py
-
+cd ../
 # compile roi_align (based on Caffe2's implementation)
-cd ../../
-cd modeling/roi_xfrom/roi_align/src
+cd roi_align/src
 echo "Compiling roi align kernels by nvcc..."
 nvcc -c -o roi_align_kernel.cu.o roi_align_kernel.cu \
 	 -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../
 python build.py
+cd ../
