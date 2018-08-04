61eea2a7202cd1ef1f61861f7b1554c657fe604f
Donny You <youansheng@gmail.com>
2018-08-04 10:02:53 +0800

    voc det generator.

--- datasets/det/voc/voc_det_generator.py
+++ datasets/det/voc/voc_det_generator.py
@@ -74,29 +74,44 @@ class VocDetGenerator(object):
 
     def generate_label(self):
         file_count = 0
-        with open(os.path.join(self.args.root_dir, 'VOC2007/ImageSets/Main/trainval.txt'), 'r') as train_stream:
-            for img_name in train_stream.readlines():
-                img_name = img_name.rstrip()
-                label_file = '{}.xml'.format(img_name)
-                file_count += 1
-                tree_dict = self._get_info_tree(label_file, dataset='VOC2007')
-                fw = open(os.path.join(self.train_json_dir, '{}.json'.format(img_name)), 'w')
-                fw.write(json.dumps(tree_dict))
-                fw.close()
-                shutil.copy(os.path.join(self.args.root_dir, 'VOC2007/JPEGImages', '{}.jpg'.format(img_name)),
-                            os.path.join(self.train_image_dir, '{}.jpg'.format(img_name)))
+        if self.args.dataset in ['VOC07, VOC07+12', 'VOC07++12']:
+            with open(os.path.join(self.args.root_dir, 'VOC2007/ImageSets/Main/trainval.txt'), 'r') as train_stream:
+                for img_name in train_stream.readlines():
+                    img_name = img_name.rstrip()
+                    label_file = '{}.xml'.format(img_name)
+                    file_count += 1
+                    tree_dict = self._get_info_tree(label_file, dataset='VOC2007')
+                    fw = open(os.path.join(self.train_json_dir, '{}.json'.format(img_name)), 'w')
+                    fw.write(json.dumps(tree_dict))
+                    fw.close()
+                    shutil.copy(os.path.join(self.args.root_dir, 'VOC2007/JPEGImages', '{}.jpg'.format(img_name)),
+                                os.path.join(self.train_image_dir, '{}.jpg'.format(img_name)))
 
-        with open(os.path.join(self.args.root_dir, 'VOC2012/ImageSets/Main/trainval.txt'), 'r') as train_stream:
-            for img_name in train_stream.readlines():
-                img_name = img_name.rstrip()
-                label_file = '{}.xml'.format(img_name)
-                file_count += 1
-                tree_dict = self._get_info_tree(label_file, dataset='VOC2012')
-                fw = open(os.path.join(self.train_json_dir, '{}.json'.format(img_name)), 'w')
-                fw.write(json.dumps(tree_dict))
-                fw.close()
-                shutil.copy(os.path.join(self.args.root_dir, 'VOC2012/JPEGImages', '{}.jpg'.format(img_name)),
-                            os.path.join(self.train_image_dir, '{}.jpg'.format(img_name)))
+        if self.args.dataset in ['VOC07+12', 'VOC07++12', 'VOC12']:
+            with open(os.path.join(self.args.root_dir, 'VOC2012/ImageSets/Main/trainval.txt'), 'r') as train_stream:
+                for img_name in train_stream.readlines():
+                    img_name = img_name.rstrip()
+                    label_file = '{}.xml'.format(img_name)
+                    file_count += 1
+                    tree_dict = self._get_info_tree(label_file, dataset='VOC2012')
+                    fw = open(os.path.join(self.train_json_dir, '{}.json'.format(img_name)), 'w')
+                    fw.write(json.dumps(tree_dict))
+                    fw.close()
+                    shutil.copy(os.path.join(self.args.root_dir, 'VOC2012/JPEGImages', '{}.jpg'.format(img_name)),
+                                os.path.join(self.train_image_dir, '{}.jpg'.format(img_name)))
+
+        if self.args.dataset in ['VOC07++12']:
+            with open(os.path.join(self.args.root_dir, 'VOC2007/ImageSets/Main/test.txt'), 'r') as train_stream:
+                for img_name in train_stream.readlines():
+                    img_name = img_name.rstrip()
+                    label_file = '{}.xml'.format(img_name)
+                    file_count += 1
+                    tree_dict = self._get_info_tree(label_file)
+                    fw = open(os.path.join(self.train_json_dir, '{}.json'.format(img_name)), 'w')
+                    fw.write(json.dumps(tree_dict))
+                    fw.close()
+                    shutil.copy(os.path.join(self.args.root_dir, 'VOC2007/JPEGImages', '{}.jpg'.format(img_name)),
+                                os.path.join(self.train_image_dir, '{}.jpg'.format(img_name)))
 
         with open(os.path.join(self.args.root_dir, 'VOC2007/ImageSets/Main/test.txt'), 'r') as train_stream:
             for img_name in train_stream.readlines():
@@ -117,6 +132,8 @@ if __name__ == "__main__":
                         dest='save_dir', help='The directory to save the data.')
     parser.add_argument('--root_dir', default=None, type=str,
                         dest='root_dir', help='The directory of the voc root.')
+    parser.add_argument('--dataset', default=None, type=str,
+                        dest='dataset', help='The target dataset that will be generated.')
 
     args = parser.parse_args()
 
--- datasets/det/voc/voc_det_generator.sh
+++ datasets/det/voc/voc_det_generator.sh
@@ -9,7 +9,9 @@ export PYTHONPATH='/home/donny/Projects/PytorchCV'
 
 ROOT_DIR='/home/donny/DataSet/VOC/VOCdevkit'
 SAVE_DIR='/home/donny/DataSet/VOC07+12_DET'
+DATASET='VOC07+12'
 
 
 python2.7 voc_det_generator.py --root_dir $ROOT_DIR \
-                               --save_dir $SAVE_DIR
+                               --save_dir $SAVE_DIR \
+                               --dataset $DATASET
