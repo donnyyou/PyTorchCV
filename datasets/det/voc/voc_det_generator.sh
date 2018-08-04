235aac83cc257769e6ca5fc690f0cb5549bc5a18
Donny You <youansheng@gmail.com>
2018-08-04 10:27:28 +0800

    little refactor.

--- datasets/det/voc/voc_det_generator.sh
+++ datasets/det/voc/voc_det_generator.sh
@@ -8,10 +8,10 @@ export PYTHONPATH='/home/donny/Projects/PytorchCV'
 
 
 ROOT_DIR='/home/donny/DataSet/VOC/VOCdevkit'
-SAVE_DIR='/home/donny/DataSet/VOC07+12_DET'
-DATASET='VOC07+12'
+DATA_SET='VOC07+12'
+SAVE_DIR='/home/donny/DataSet/'${DATA_SET}'_DET'
 
 
 python2.7 voc_det_generator.py --root_dir $ROOT_DIR \
                                --save_dir $SAVE_DIR \
-                               --dataset $DATASET
+                               --dataset $DATA_SET
