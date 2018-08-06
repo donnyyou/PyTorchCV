f4b3dd26c6327666169dafb80605e0abedd08592
Donny You <youansheng@gmail.com>
2018-08-06 10:49:54 +0800

    caffe resnet.

--- models/backbones/resnet/resnet_models.py
+++ models/backbones/resnet/resnet_models.py
@@ -219,7 +219,13 @@ class CaffeResNet(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
-        x = self.relu(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
         x = self.maxpool(x)
 
         x = self.layer1(x)
