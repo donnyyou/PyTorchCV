11906b92fddc8ecb372c8bff0a8e511a1f0db200
Donny You <youansheng@gmail.com>
2018-08-06 11:09:20 +0800

    caffe synbn.

--- models/backbones/resnet/syncbn_resnet_models.py
+++ models/backbones/resnet/syncbn_resnet_models.py
@@ -25,7 +25,7 @@ model_urls = {
     'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
     'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
     'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/models/resnet101-imagenet.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
     'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
     'caffe_resnet101': 'https://download.pytorch.org/models/resnet101-imagenet.pth'
 }
