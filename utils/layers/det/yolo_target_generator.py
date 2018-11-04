f21629238e2326d2dc90ee076df78c60912ef2b2
Donny You <youansheng@gmail.com>
2018-11-04 15:28:19 +0800

    change numpy to torch.

--- utils/layers/det/yolo_target_generator.py
+++ utils/layers/det/yolo_target_generator.py
@@ -73,7 +73,7 @@ class YOLOTargetGenerator(object):
                     # Where the overlap is larger than threshold set mask to zero (ignore)
                     noobj_mask[b, anch_ious[0] > self.configer.get('gt', 'iou_threshold')] = 0
                     # Find the best matching anchor box
-                    best_n = np.argmax(anch_ious, axis=1)
+                    best_n = torch.argmax(anch_ious, dim=1)
 
                     # Masks
                     obj_mask[b, best_n, gj, gi] = 1
